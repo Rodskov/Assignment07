202fdcda5f2951b11ec1db9b822001c095efb145
Justin Carl de Guia <justincarlcdeguia@iskolarngbayan.pup.edu.ph>
2021-11-27 12:24:38 +0800

    added user input function

---
+++ charCounter.py
@@ -0,0 +1,10 @@
+#       Character Counter
+#   This program will count the number of words,
+#   vowels, and consonants in the sentence that
+#   will be input by the user.
+
+def askUser():
+    userSentence = input("What would be your sentence?: ")
+    return userSentence
+
+userSentence = 
