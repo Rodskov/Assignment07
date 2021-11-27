1d0bdcaa700e899f5002a8d73145f358828f47f7
Justin Carl de Guia <justincarlcdeguia@iskolarngbayan.pup.edu.ph>
2021-11-27 12:31:43 +0800

    added vowel and consonants as reference

--- charCounter.py
+++ charCounter.py
@@ -3,8 +3,13 @@
 #   vowels, and consonants in the sentence that
 #   will be input by the user.
 
+#Step 1: Ask the user to input a sentence.
 def askUser():
     userSentence = input("What would be your sentence?: ")
     return userSentence
 
-userSentence = 
+userSentence = askUser()
+
+#Step 2: Add references for vowels and consonants.
+vowels = ['aeiou']
+consonants = ['bcdfghjklmnpqrstvwxyz']
