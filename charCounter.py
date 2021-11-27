640aa47727968aab4c856078b8141e034bfa5b1a
Justin Carl de Guia <justincarlcdeguia@iskolarngbayan.pup.edu.ph>
2021-11-27 13:38:48 +0800

    added vowel and consonant counter; also added outputs

--- charCounter.py
+++ charCounter.py
@@ -5,21 +5,34 @@
 
 #Step 1: Ask the user to input a sentence.
 def askUser():
-    userSentence = input("What would be your sentence?: ")
+    userSentence = str(input("What would be your sentence?: "))
     return userSentence
 
 userSentence = askUser()
 
 #Step 2: Add references for vowels and consonants.
-vowels = ['aeiouAEIOU']
-consonants = ['bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ']
+vowelsList = ["a", "e", "i", "o", "u"]
+consonantsList = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "y", "z"]
 
 #Step 3: Do the counting program.
-
 ##Word Counter
 def wordCounter():
     splitSentence = userSentence.split()
     count = len(splitSentence)
     return count
 wordCount = wordCounter()
-print(wordCount)
+##Vowel Counter
+vowelCount = 0
+for l in userSentence:
+    if l.lower() in vowelsList:
+        vowelCount += 1
+#Consonant Counter
+consonantCount = 0
+for l in userSentence:
+    if l.lower() in consonantsList:
+        consonantCount += 1
+
+#Step 4: Show the output
+def userKnow():
+    print(f"Number of words: {wordCount}\nNumber of vowels: {vowelCount}\nNumber of consonants: {consonantCount}")
+userKnow()
